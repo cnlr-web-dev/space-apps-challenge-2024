bbf728169cf5dbd9cf622c08207ef97d0886fe9d
Moldytzu <40271862+Moldytzu@users.noreply.github.com>
2024-10-06 21:36:25 +0300

    schimba server

--- src/Three.js
+++ src/Three.js
@@ -116,7 +116,7 @@ function Three() {
     ]
 
     async function update_json_api() {
-        const api_gateway = "http://192.168.0.24:3000/";
+        const api_gateway = "http://89.137.132.40:3000/";
         const response = await fetch(api_gateway);
         const json = await response.json();
 
