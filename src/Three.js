080c58266be51466d94c5c1444a930c1c85fc983
Moldytzu <40271862+Moldytzu@users.noreply.github.com>
2024-10-06 12:00:38 +0300

    reactiveaza stelele

--- src/Three.js
+++ src/Three.js
@@ -202,17 +202,17 @@ function Three() {
                     const starGeometry = new THREE.BufferGeometry();
                     const starMaterial = new THREE.PointsMaterial({ color: 0xffffff });
                     const starVertices = [];
-                    for (let i = 0; i < 10000; i++) {
-                        const x = (Math.random() - 0.5) * 20000000000;
-                        const y = (Math.random() - 0.5) * 20000000000;
-                        const z = (Math.random() - 0.5) * 20000000000;
+                    for (let i = 0; i < 5000; i++) {
+                        const x = (Math.random() - 0.5) * 2000000000000;
+                        const y = (Math.random() - 0.5) * 2000000000000;
+                        const z = (Math.random() - 0.5) * 2000000000000;
                         starVertices.push(x, y, z);
                     }
                     starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVertices, 3));
                     const stars = new THREE.Points(starGeometry, starMaterial);
                     scene.add(stars);
                 }
-                //createStars();
+                createStars();
 
                 function animate() {
                     const date = new Date();
